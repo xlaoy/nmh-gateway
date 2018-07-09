9908362567937d1e121b8ebaf1d83ff36adcc79b
xlaoy <768881412@qq.com>
2018-07-09 16:27:57 +0800

    redis

--- src/main/java/com/xlaoy/nmhgateway/NmhGatewayApplication.java
+++ src/main/java/com/xlaoy/nmhgateway/NmhGatewayApplication.java
@@ -1,7 +1,7 @@
 package com.xlaoy.nmhgateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableEurekaClient
 @EnableCircuitBreaker
-@SpringCloudApplication
+@SpringBootApplication(scanBasePackages = "com.xlaoy")
 public class NmhGatewayApplication {
 
     public static void main(String[] args) {
